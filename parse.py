3faecf42e911986f7d406a80811a33b2e51cea41
WanxinXu27 <36319370+WanxinXu27@users.noreply.github.com>
2018-04-06 20:58:17 -0500

    Initial commit

---
+++ parse.py
@@ -0,0 +1,24 @@
+import csv
+import os
+
+def parse(filename):
+  '''
+  takes a filename and returns attribute information and all the data in array of dictionaries
+  '''
+  # initialize variables
+
+  out = []  
+  csvfile = open(filename,'rb')
+  fileToRead = csv.reader(csvfile)
+
+  headers = fileToRead.next()
+
+  # iterate through rows of actual data
+  for row in fileToRead:
+    out.append(dict(zip(headers, row)))
+
+  return out
+
+if __name__ == "__main__":
+  out = parse("house_votes_84.data")
+  os.system("pause")
